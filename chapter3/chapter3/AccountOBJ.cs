773f2315de285424cf64428501a33a934d214aa2
yiğit <82784013+ozaiithejava@users.noreply.github.com>
2025-12-02 01:32:23 +0300

    Add files via upload

---
+++ chapter3/chapter3/AccountOBJ.cs
@@ -0,0 +1,20 @@
+﻿namespace chapter3;
+
+public class AccountOBJ
+{
+    public string AccountName { get; set; }   
+    public string AccountState { get; set; } 
+
+    public AccountOBJ(string accountName, string accountState)
+    {
+        this.AccountName = accountName;
+        this.AccountState = accountState;
+    }
+
+    public void setName()
+    {
+        
+    }
+    
+    
+}
---
+++ chapter3/chapter3/Main.cs
@@ -0,0 +1,13 @@
+﻿namespace chapter3;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        AccountOBJ ozaiiAccount = new AccountOBJ("ozaiithejava", "ServerSideDeveloper");
+        AccountOBJ emrullahAccount = new AccountOBJ("emrullah", "Boss");
+        
+        Console.WriteLine(ozaiiAccount.AccountName);
+        Console.WriteLine(emrullahAccount.AccountState);
+    }
+}
