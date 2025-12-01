680fac5de73dc3663be9ea8f87aef1e0b5859bfe
yiğit <82784013+ozaiithejava@users.noreply.github.com>
2025-12-02 01:21:26 +0300

    Add files via upload

---
+++ Chapter2/Chapter2/Form1.Designer.cs
@@ -0,0 +1,40 @@
+﻿
+// @project: Chapter2 - Windows Forms Application
+// @author: ozaiithejava
+// @version: 1.0
+// @date: 2025
+
+
+namespace Chapter2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer Generated Code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+            this.Name = "Form1";
+            
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Chapter2/Chapter2/Form1.cs
@@ -0,0 +1,9 @@
+namespace Chapter2;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
